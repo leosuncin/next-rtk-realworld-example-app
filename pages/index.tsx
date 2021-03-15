329832adfc0ef0bb21bbc779f57b9778db2cd08b
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-03-15 12:13:26 -0600

    chore: remove boilerplate code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,5 @@
 import type { NextPage } from 'next';
-import NextLink from 'next/link';
 
-import Layout from '@app/components/layout';
-
-const IndexPage: NextPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <NextLink href="/about">
-        <a>About</a>
-      </NextLink>
-    </p>
-  </Layout>
-);
+const IndexPage: NextPage = () => <h1>Hello Next.js 👋</h1>;
 
 export default IndexPage;
