c37959e2749a41880047b9c747230ca3b4d42857
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-03-15 12:04:48 -0600

    feat: use the styles from Conduit

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <link rel="stylesheet" href="//demo.productionready.io/main.css" />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
