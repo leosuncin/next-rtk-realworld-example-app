7f173a6ab9d9ab80d9370f0c826769adfe634776
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-03-15 12:44:30 -0600

    chore: define NEXT_PUBLIC_API_ROOT variable

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    readonly NEXT_PUBLIC_API_ROOT: string;
+  }
+}
