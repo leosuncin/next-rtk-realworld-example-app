ee3c3382ba702d4bbcb8e9f06ef0f5d906c0ff2e
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-03-15 12:59:47 -0600

    feat(conduit): add tags slice
    Add slice to store.
    Fetch tags.

--- store/index.ts
+++ store/index.ts
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import articlesSlice, {
   ARTICLES_FEATURE_KEY,
 } from '@app/store/slices/articles.slice';
+import tagsSlice, { TAGS_FEATURE_KEY } from '@app/store/slices/tags.slice';
 
 const store = configureStore({
   devTools: true,
   reducer: {
     [ARTICLES_FEATURE_KEY]: articlesSlice,
+    [TAGS_FEATURE_KEY]: tagsSlice,
   },
 });
 
