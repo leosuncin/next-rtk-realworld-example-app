8f196dcc10d5ff041288c7698e8fcde1dfad5f3d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-03-15 22:59:33 -0600

    chore: load .env files during test

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,8 @@
 /* eslint-env jest */
 // eslint-disable-next-line import/no-unassigned-import
 import '@testing-library/jest-dom';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { loadEnvConfig } from '@next/env';
+
+loadEnvConfig(__dirname, true, { info: () => null, error: console.error });
