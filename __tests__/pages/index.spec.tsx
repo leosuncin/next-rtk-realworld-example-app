bbe408db08f817b5cf2551b1646162f992dde007
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-03-15 22:09:15 -0600

    test(conduit): wrap page with providers

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,12 +1,42 @@
 import { render, screen } from '@testing-library/react';
+import { RouterContext } from 'next/dist/next-server/lib/router-context';
+import type { NextRouter } from 'next/router';
+import { Provider } from 'react-redux';
 
 import IndexPage from '@app/pages/index';
+import store from '@app/store';
 
 describe('<IndexPage />', () => {
   it('should render', () => {
-    const { baseElement } = render(<IndexPage />);
+    const routerMocked: jest.Mocked<NextRouter> = {
+      pathname: '/',
+      route: '/',
+      isPreview: false,
+      query: {},
+      asPath: '/',
+      basePath: '',
+      isFallback: false,
+      isReady: true,
+      isLocaleDomain: true,
+      events: { emit: jest.fn(), off: jest.fn(), on: jest.fn() },
+      push: jest.fn(),
+      replace: jest.fn(),
+      reload: jest.fn(),
+      back: jest.fn(),
+      prefetch: jest.fn().mockResolvedValue(undefined),
+      beforePopState: jest.fn(),
+    };
+    const { baseElement } = render(
+      <RouterContext.Provider value={routerMocked}>
+        <Provider store={store}>
+          <IndexPage />
+        </Provider>
+      </RouterContext.Provider>,
+    );
 
     expect(baseElement).toBeInTheDocument();
-    expect(screen.getByText(/hello next.js/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/a place to share your knowledge/i),
+    ).toBeInTheDocument();
   });
 });
